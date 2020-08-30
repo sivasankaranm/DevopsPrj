6c21fea37cc4a4fb75c8c31fa1c5c860dabd4e00
sivasankaran <sivazeee82@gmail.com>
2020-08-30 19:45:41 +0530

    First Commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/")
+
+public class HelloController {
+
+	@GetMapping(path="/hello",produces = "application/json")
+	public String getHello(){
+		return "Hello World";
+	}
+	
+}
